d60457e84c615583f6b09d5273a1907373bc0331
bombbom <69651890+Armaggedom@users.noreply.github.com>
2021-06-15 14:02:29 -0300

    Add files via upload

--- assistent/main.cpp
+++ assistent/main.cpp
@@ -10,12 +10,21 @@
 #include <allegro5/allegro_native_dialog.h>
 //Namespaces
 using namespace Basic_Color_Structs;
+//namespace Window_Create_Form (declared using '::')
+//Version
+char version[]="1.3.1-ALPHA-DEBUG";
+//Allegro Conversor
+Window_Create_Form::WINDOW_DEFINE win;
+//Global Funcion
+bool Error(void) {
+    return true;
+}
 void ComandCMDOutput(void) {
     //CMD Output
     for(int i=0; i<17; i++) {
         std::cout<<"-";
         };
-    std::cout<<"\nDev: Bombbom\nVersion:1.2.1 ALPHA-DEBUG"<<std::endl;
+    std::cout<<"\nDev: Bombbom\nVersion: "<<version<<std::endl;
     for(int i=0; i<17; i++) {
         std::cout<<"-";
     };
@@ -27,41 +36,36 @@ int main(void) {
     //CMD Output
     void ComandCMDOutput(void);
     void RenderDisplay(void);
+    int Text_Builder(void);
     void Obj_Builder(void);
     //Call CMD Output
     ComandCMDOutput();
     //Allegro define
     ALLEGRO_EVENT_QUEUE *event=NULL;
     ALLEGRO_DISPLAY *display=NULL;
-    ALLEGRO_FONT *font=NULL;
-    //Allegro Conversor
-    Window_Create_Form::WINDOW_DEFINE win;
-    //Allegro ERROR
+    //Allegro init
     if(!al_init()) {
-        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ERRO: ALLEGRO fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ALLEGRO fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     };
+    //Init font addon
     al_init_font_addon();
+    //Init ttf addon(font function in .TTF)
     if(!al_init_ttf_addon()) {
-        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ERRO: TTF fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "TTF fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     };
+    //Init Primitive_addon(Color and bitmap)
     if(!al_init_primitives_addon()) {
-        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ERRO: ALLEGRO Graphic fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ALLEGRO Graphic fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     };
-    //create display
+    //Create display
     display=al_create_display(win.WindowLenght, win.WindowHight);
     if(!display) {
-        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ERRO: Display fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "Display fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     };
-    //Font load
-    font=al_load_font("Anger_Management.ttf", 45, 0);
-    if(!font) {
-        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ERRO: TTF not found", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-        return -1;
-    }
     //Define Background
     BACKGROUND_DEFINE background;
     //Define Background Color
@@ -71,37 +75,54 @@ int main(void) {
     //create event
     event=al_create_event_queue();
     al_register_event_source(event, al_get_display_event_source(display));
-    //Allegro instal
+    //Allegro install
     if(!al_install_mouse()) {
-        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "ERRO: Mouse instal fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "Mouse install fail", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     };
-
     //Draw text
-    BUTTON_FONT fontS;
-    al_draw_text(font, al_map_rgb(fontS.font_colorR, fontS.font_colorG, fontS.font_colorB), fontS.x, fontS.y, ALLEGRO_ALIGN_LEFT, "Esquerda");
-
-    while(!stop){
-        //main command
-        RenderDisplay();
-        //event declaration
-        ALLEGRO_EVENT ev;
-        al_wait_for_event(event, &ev);
-        //event
-        if(ev.type==ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            stop=true;
-        };
+    if(Text_Builder()==-1) {
+        al_destroy_display(display);
+        al_show_native_message_box(NULL, "ERROR", "ERROR: ", "TTF not found", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
     };
-    //end command
-    al_destroy_display(display);
-    al_destroy_event_queue(event);
-    al_destroy_font(font);
-    std::cout<<"process exited"<<std::endl;
-    return 0;
+    //Main loop
+
+        while(!stop){
+            //Render function
+            RenderDisplay();
+            //event declaration
+            ALLEGRO_EVENT ev;
+            al_wait_for_event(event, &ev);
+            //event
+            if(ev.type==ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                stop=true;
+            }
+        };
+        //end command
+        al_destroy_display(display);
+        al_destroy_event_queue(event);
+        std::cout<<"process exited"<<std::endl;
+        return 0;
 }
 void Obj_Builder(void) {
     BUTTON_STYLE button;
     al_draw_filled_rectangle(button.x1, button.x2, button.y1, button.y2, al_map_rgb(button.r, button.g, button.b));
+    al_draw_filled_rectangle(button.x1, button.addSpacex*button.x2, button.y1, button.addSpacey, al_map_rgb(button.r, button.g, button.b));
+}
+int Text_Builder(void) {
+    ALLEGRO_FONT *font=NULL;
+    //Font load
+    font=al_load_font("Anger_Management.ttf", 45, 0);
+    if(!font) {
+        return -1;
+    };
+    BUTTON_FONT fontS;
+    al_draw_text(font, al_map_rgb(fontS.font_colorR, fontS.font_colorG, fontS.font_colorB), fontS.x, fontS.y, ALLEGRO_ALIGN_LEFT, "Esquerda");
+    al_draw_text(font, al_map_rgb(fontS.font_colorR, fontS.font_colorG, fontS.font_colorB), fontS.x, fontS.spaceText*fontS.y, ALLEGRO_ALIGN_LEFT, "Esquerda");
+    al_draw_textf(font, al_map_rgb(fontS.font_colorR, fontS.font_colorG, fontS.font_colorB), win.WindowLenght, win.WindowHight-50, ALLEGRO_ALIGN_RIGHT, "V: %s", version);
+    al_destroy_font(font);
+    return 0;
 }
 void RenderDisplay(void) {
     al_flip_display();
--- assistent/object.h
+++ assistent/object.h
@@ -1,6 +1,5 @@
 #ifndef OBJECT_H
 #define OBJECT_H
-
 namespace Window_Create_Form {
     struct WINDOW_DEFINE {
         int WindowHight=420;
@@ -15,17 +14,20 @@ namespace Basic_Color_Structs {
     };
     //button obj
     struct BUTTON_STYLE {
-        int x1=10;
-        int x2=35;
-        int y1=200;
-        int y2=100;
-       int r=255, g=255, b=0;
+        float x1=10;
+        float x2=35;
+        float y1=200;
+        float y2=100;
+        float addSpacex=3.5;
+        float addSpacey=y2-x2+x2*addSpacex;
+        int r=255, g=255, b=0;
     };
     struct BUTTON_FONT {
         //not in use
         int font_colorR=0, font_colorG=0, font_colorB=0;
         int x=10;
         int y=40;
+        float spaceText=3.2;
     };
 };
 #endif
